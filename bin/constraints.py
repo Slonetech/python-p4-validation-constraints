3b69a3afb764188555082cd2690beedd309e6fb7
Slonetech <kengeregordon@gmail.com>
2023-09-25 12:06:47 +0300

    validation-constraint-done

--- bin/constraints.py
+++ bin/constraints.py
@@ -14,7 +14,7 @@ class Patient(base):
     __tablename__ = 'patient'
     name = Column(String(length=50), primary_key=True)
     birth_year = Column(Integer,
-                        CheckConstraint('birth_year < 2023'),
+                         CheckConstraint('birth_year < 2023'),
                         nullable=False)
     death_year = Column(Integer)
         # Constraint defined at the Table level
